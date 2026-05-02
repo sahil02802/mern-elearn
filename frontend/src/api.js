66fe56049e3aa1fc7414742b7de990516f14e7c1
sahil02802 <sahil02817@gmail.com>
2026-05-03 01:36:12 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { clearSession, getToken } from "./auth";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_URL || "https://mern-elearn-backend.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
